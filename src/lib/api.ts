0d803d2003f3d26c3211157b46c490cecd319e21
nohanakamatsuke <nohana115ron@icloud.com>
2024-08-05 14:43:32 +0900

    axiosを使用してresas APIから都道府県情報と人口種別情報をfetch

---
+++ src/lib/api.ts
@@ -0,0 +1,50 @@
+import { populationType } from "@/types";
+import axios from "axios";
+
+const API_BASE_URL = "https://opendata.resas-portal.go.jp/api/v1";
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { "X-API-KEY": API_KEY },
+});
+
+export const fetchPrefectures = async () => {
+  try {
+    const response = await axiosInstance.get("/prefectures");
+    return response.data.result;
+  } catch (error) {
+    console.error("Failed to fetch prefectures:", error);
+    return [];
+  }
+};
+
+interface PopulationData {
+  label: string;
+  data: { year: number; value: number }[];
+}
+
+export const fetchPopulationData = async (
+  prefCode: number,
+  populationType: populationType
+) => {
+  try {
+    const response = await axiosInstance.get(
+      `/population/composition/perYear?prefCode=${prefCode}`
+    );
+    const prefData = response.data.result.data.find(
+      (d: PopulationData) => d.label === populationType
+    );
+    return {
+      prefCode,
+      prefName: response.data.result.prefName,
+      data: prefData.data,
+    };
+  } catch (error) {
+    console.error(
+      `Failed to fetch population data for prefecture ${prefCode}:`,
+      error
+    );
+    return null;
+  }
+};
